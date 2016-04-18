c486c7a911af80945c3ea0e7a31abb2950dc634c
MengHongYuan <menghongyuan@qq.com>
2016-04-18 17:10:41 +0800

    angular

--- php/chk.php
+++ php/chk.php
@@ -1,9 +1,9 @@
 <?php
-    function checkNum(num){
-        return (num%2)?TRUE:FALSE;
+    function checkNum($num){
+        return ($num%2)?true:false;
     }
     $num=$_GET['n'];
-    if(checkNum===TRUE){
+    if(checkNum($num)){
         echo '奇数';
     }else{
         echo '偶数';
