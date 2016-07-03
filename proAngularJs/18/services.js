07dcfe5bc2b8f25c84d2204c74ae37714adf9fd9
MengHongYuan <menghongyuan@qq.com>
2016-07-03 16:49:19 +0800

    angular

--- proAngularJs/18/services.js
+++ proAngularJs/18/services.js
@@ -27,3 +27,35 @@ angular.module('customServices',[]).factory('logService',function(){
         }
     }
 });
+
+angular.module('customService',[]).provider('logService',function(){
+    var counter=true,debug=true;
+    return {
+        messageCounterEnabled:function(setting){
+            if(angular.isDefined(setting)){
+                counter=setting;
+                return this;
+            }else{
+                return counter;
+            }
+        },
+        debugEnabled:function(setting){
+            if(angular.isDefined(setting)){
+                debug=setting;
+                return this;
+            }else{
+                return debug;
+            }
+        },
+        $get:function(){
+            return {
+                messageCount:0,
+                log:function(msg){
+                    if(debug){
+                        console.log("(LOG"+(counter?"+"+this.messageCount++ +")":")")+msg);
+                    }
+                }
+            }
+        }
+    }
+});
